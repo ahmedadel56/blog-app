2f8730a6aa85f88200374e75ec0a3378ef374975
ahmedadel56 <ahmdadl563@gmail.com>
2022-02-01 20:23:46 +0200

    Do required changes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,7 @@
 class CommentsController < ApplicationController
+  def new
+    @comment = Comment.new
+  end
   def create
     @post = Post.find(params[:post_id])
     new_comment = current_user.comments.new(
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     end
 
     resources :posts do
-      resources :comments, only: [:create]
+      resources :comments, only: [:create, :new]
       resources :likes, only: [:create]
     end
   end
