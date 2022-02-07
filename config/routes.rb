4d19ec6f8d5b9f3982b9b780f090518054b530dc
ahmedadel56 <ahmdadl563@gmail.com>
2022-02-08 00:31:33 +0200

    Add all stuff about devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'users#index'
+
+  devise_scope :user do
+    # write all your routes inside this block
+    root 'users#index'
     resources :users, only: [:index, :show] do
       resources :posts, only: [:index, :new, :create, :show]
     end
-
+  
     resources :posts do
-      resources :comments, only: [:create, :new]
+      resources :comments, only: [:create]
       resources :likes, only: [:create]
     end
+  end
   end
